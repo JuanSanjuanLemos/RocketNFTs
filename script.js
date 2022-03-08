b5b0b2688752237361d5ce268917cc04289e8629
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2022-03-07 21:53:36 -0300

    :iphone: Work on responsive
    #

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function topScroll(){
-    window.scrollTo({ top: 0, behavior: "smooth" })
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+const menuMobile = 
+    `<div>
+        <h2>Comprar NFT</h2>
+        <h2>Sobre</h2>
+        <h2>FAQ</h2>
+        <button class="button-conect">Conectar carteira</button>
+    </div>`;
+
+function clickMenu(){
+    let nav = document.querySelector('nav');
+    nav.classList.toggle('active');
 }
